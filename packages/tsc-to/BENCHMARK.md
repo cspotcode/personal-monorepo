afa10dc4579362998e812c72c775cf9514164703
Andrew Bradley <cspotcode@gmail.com>
2019-01-31 16:54:35 -0500

    fix tsc-to newlines in shell script

---
+++ packages/tsc-to/BENCHMARK.md
@@ -0,0 +1,7 @@
+Invoke babel like this:
+
+    ./node_modules/.bin/babel src --out-dir babel-out --extensions ".ts,.tsx" --source-maps
+
+Invoke tsc-to like this:
+
+    node ./dist/bin/tsc-to.js
---
+++ packages/tsc-to/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function (api) {
+    api.cache(true);
+  
+    return {
+        presets: [
+            "@babel/env",
+            "@babel/typescript"
+        ],
+        plugins: [
+            "@babel/proposal-class-properties",
+            "@babel/proposal-object-rest-spread"
+        ]
+    };
+}
