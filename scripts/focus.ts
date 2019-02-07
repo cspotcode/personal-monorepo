698ef72489c32da065107ce8d84d7d2910d9bcad
Andrew Bradley <cspotcode@gmail.com>
2019-02-07 02:18:13 -0500

    updates

--- scripts/focus.ts
+++ scripts/focus.ts
@@ -6,11 +6,13 @@ import { workspaceFilename } from './fixup';
 
 const focusOn = process.argv.slice(2).map(v => Path.basename(v));
 
+const always = ['__ROOT__', '__template__'];
+
 patchJsonFile(workspaceFilename, (pkg) => {
     const {folders = [], _folders = []} = pkg;
     const allFolders = sortBy([...folders, ..._folders], v => v.name);
     if(focusOn.length) {
-        pkg.folders = allFolders.filter(v => focusOn.includes(v.name) || v.name === '__ROOT__');
+        pkg.folders = allFolders.filter(v => focusOn.includes(v.name) || always.includes(v.name));
         pkg._folders = allFolders.filter(v => !pkg.folders.includes(v));
     } else {
         pkg.folders = allFolders;
